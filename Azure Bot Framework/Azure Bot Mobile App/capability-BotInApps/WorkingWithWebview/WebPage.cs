c1d60a74fad87962d75222cd9df28ce8fc53191d
Cheah Eng Teong <ceteongvanness@hotmail.com>
2018-10-27 20:10:02 +0800

    Azure Bot Mobile App

---
+++ Azure Bot Framework/Azure Bot Mobile App/capability-BotInApps/WorkingWithWebview/WebPage.cs
@@ -0,0 +1,17 @@
+﻿namespace WorkingWithWebview
+{
+    using Xamarin.Forms;
+
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+
+            browser.Source = "https://webchat.botframework.com/embed/etdemobot?s=evr56s4zHLE.cwA.sY0.CSL_fVJpTrhMr8JeFqOXMsfNi8sFNSW3_Pcud8_kuYw";
+            //browser.Source = "https://webchat.botframework.com/embed/<INCLUDE YOUR BOT WEBCHAT URL>";
+
+            this.Content = browser;
+        }
+    }
+}
